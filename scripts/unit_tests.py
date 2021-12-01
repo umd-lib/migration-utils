cd6ec1906705d5b0442af6c2f90ae4608955bc43
David P. Steelman <dsteelma@umd.edu>
2021-12-01 10:14:55 -0500

    LIBAVALON-166. Refactored code and added "ordering" constraints to output
    Refactored the bibref conversion code into a "BibRefToTextConverter"
    class, as the code was getting fairly complicated, and wanted to clarify
    the code by organizing it into its own class.
    
    As part of that refactoring, moved the "collapse_whitespace_nodes" and
    "get_text" utility methods into an "XmlUtils" class. This way, the
    "BibRefToTextConverter" class is dependent on another class, not the
    functions in the script.
    
    https://issues.umd.edu/browse/LIBAVALON-166

--- scripts/unit_tests.py
+++ scripts/unit_tests.py
@@ -4,74 +4,93 @@
 import unittest
 
 from xml.dom.minidom import parseString
-from avalon import bibref_to_note_text, collapse_whitespace_nodes
+from avalon import BibRefToTextConverter, XmlUtils
 
 
-class TestBibRefToNoteText(unittest.TestCase):
-    '''Test cases for the 'bibref_to_note_text' method in avalon.py'''
+class TestBibRefToTextConverter(unittest.TestCase):
+    '''Test cases for the 'bib_ref_to_note_text' method in avalon.py'''
 
     @staticmethod
     def to_element(xml: str):
         '''Converts the given string into minidom Element'''
         doc = parseString(str(xml))
-        collapse_whitespace_nodes(doc)
+        XmlUtils.collapse_whitespace_nodes(doc)
         doc_element = doc.documentElement
         return doc_element
 
-    def test_bibref_with_no_children(self):
+    def test_bib_ref_with_no_children(self):
         xml = '<bibRef />'
-        bibref = self.to_element(xml)
-        self.assertEqual('', bibref_to_note_text(bibref))
+        bib_ref = self.to_element(xml)
+        self.assertEqual('', BibRefToTextConverter.as_text(bib_ref))
 
         xml = '<bibRef></bibRef>'
-        bibref = self.to_element(xml)
-        self.assertEqual('', bibref_to_note_text(bibref))
+        bib_ref = self.to_element(xml)
+        self.assertEqual('', BibRefToTextConverter.as_text(bib_ref))
 
-    def test_bibref_with_only_title(self):
+    def test_bib_ref_with_only_title(self):
         xml = '<bibRef>\n        <title type="main">Madrigal Singers</title>\n      </bibRef>'
-        bibref = self.to_element(xml)
-        self.assertEqual('Madrigal Singers', bibref_to_note_text(bibref))
+        bib_ref = self.to_element(xml)
+        self.assertEqual('Madrigal Singers', BibRefToTextConverter.as_text(bib_ref))
 
-    def test_bibref_with_title_and_accession(self):
+    def test_bib_ref_with_title_and_accession(self):
         xml = '<bibRef><title type="main">University of Maryland\xa0Football\xa0Heritage\xa0Film\xa0Collection</title>\n<bibScope type="accession">2011-166</bibScope></bibRef>'
-        bibref = self.to_element(xml)
-        self.assertEqual('University of Maryland\xa0Football\xa0Heritage\xa0Film\xa0Collection, Accession 2011-166', bibref_to_note_text(bibref))
+        bib_ref = self.to_element(xml)
+        self.assertEqual('University of Maryland\xa0Football\xa0Heritage\xa0Film\xa0Collection, Accession 2011-166', BibRefToTextConverter.as_text(bib_ref))
 
-    def test_bibref_with_title_and_box_and_accession(self):
+    def test_bib_ref_with_title_and_box_and_accession(self):
         xml = '<bibRef>\n        <title type="main">WMUC Archives</title>\n        <bibScope type="box">1</bibScope>\n        <bibScope type="accession">2011-084</bibScope>\n      </bibRef>'
-        bibref = self.to_element(xml)
-        self.assertEqual('WMUC Archives, Box 1, Accession 2011-084', bibref_to_note_text(bibref))
+        bib_ref = self.to_element(xml)
+        self.assertEqual('WMUC Archives, Accession 2011-084, Box 1', BibRefToTextConverter.as_text(bib_ref))
 
-    def test_bibref_with_empty_title_and_empty_box_and_accession(self):
+    def test_bib_ref_with_empty_title_and_empty_box_and_accession(self):
         xml = '<bibRef>\n                <title type="main"/>\n                <bibScope type="box"/>\n                <bibScope type="accession">2011-084</bibScope>\n              </bibRef>'
-        bibref = self.to_element(xml)
-        self.assertEqual('Accession 2011-084', bibref_to_note_text(bibref))
+        bib_ref = self.to_element(xml)
+        self.assertEqual('Accession 2011-084', BibRefToTextConverter.as_text(bib_ref))
 
-    def test_bibref_with_title_and_series_and_box_and_folder_and_item(self):
+    def test_bib_ref_with_title_and_series_and_box_and_folder_and_item(self):
         xml = '<bibRef>\n        <title type="main">Jackson R. Bryer Interviews Collection</title>\n        <bibScope type="series">1</bibScope>\n        <bibScope type="box">1</bibScope>\n        <bibScope type="folder">1</bibScope>\n        <bibScope type="item">5.0</bibScope>\n      </bibRef>'
-        bibref = self.to_element(xml)
-        self.assertEqual('Jackson R. Bryer Interviews Collection, Series 1, Box 1, Folder 1, Item 5.0', bibref_to_note_text(bibref))
+        bib_ref = self.to_element(xml)
+        self.assertEqual('Jackson R. Bryer Interviews Collection, Series 1, Box 1, Folder 1, Item 5.0', BibRefToTextConverter.as_text(bib_ref))
 
-    def test_bibref_with_no_title_and_accession(self):
+    def test_bib_ref_with_no_title_and_accession(self):
         xml = '<bibRef>\n                \n                \n                <bibScope type="accession">2011-084</bibScope>\n              </bibRef>'
-        bibref = self.to_element(xml)
-        self.assertEqual('Accession 2011-084', bibref_to_note_text(bibref))
+        bib_ref = self.to_element(xml)
+        self.assertEqual('Accession 2011-084', BibRefToTextConverter.as_text(bib_ref))
 
-    def test_bibref_with_empty_title(self):
+    def test_bib_ref_with_empty_title(self):
         xml = '<bibRef><title type="main"/></bibRef>'
-        bibref = self.to_element(xml)
-        self.assertEqual('', bibref_to_note_text(bibref))
+        bib_ref = self.to_element(xml)
+        self.assertEqual('', BibRefToTextConverter.as_text(bib_ref))
 
     def test_bib_with_title_and_series_and_accession_and_subseries_and_item(self):
         xml = '<bibRef><title type="main">United Brotherhood of Carpenters and Joiners America (UBC) archives</title>\n<bibScope type="series">16</bibScope>\n<bibScope type="accession">1995-94</bibScope>\n<bibScope type="subseries">2</bibScope>\n<bibScope type="item">Audio #32</bibScope></bibRef>'
-        bibref = self.to_element(xml)
-        self.assertEqual('United Brotherhood of Carpenters and Joiners America (UBC) archives, Series 16, Accession 1995-94, Subseries 2, Item Audio #32', bibref_to_note_text(bibref))
+        bib_ref = self.to_element(xml)
+        self.assertEqual('United Brotherhood of Carpenters and Joiners America (UBC) archives, Accession 1995-94, Series 16, Subseries 2, Item Audio #32', BibRefToTextConverter.as_text(bib_ref))
 
-    def test_bibref_with_title_and_accession_displays_title_first(self):
+    def test_bib_ref_with_title_and_accession_displays_title_first(self):
         xml = '<bibRef><bibScope type="accession">2011-166</bibScope>\n<title type="main">Test Title</title></bibRef>'
-        bibref = self.to_element(xml)
-        self.assertEqual('Test Title, Accession 2011-166', bibref_to_note_text(bibref))
+        bib_ref = self.to_element(xml)
+        self.assertEqual('Test Title, Accession 2011-166', BibRefToTextConverter.as_text(bib_ref))
 
+    def test_bib_ref_with_unexpected_bibscope_puts_bibscope_at_end(self):
+        xml = '<bibRef><title type="main">Test Title</title>\n<bibScope type="accession">2011-166</bibScope><bibScope type="unknownScope">abc123</bibScope>\n</bibRef>'
+        bib_ref = self.to_element(xml)
+        self.assertEqual('Test Title, Accession 2011-166, Unknownscope abc123', BibRefToTextConverter.as_text(bib_ref))
+
+    def test_bib_ref_with_unexpected_xml_puts_xml_at_end(self):
+        xml = '<bibRef><title type="main">Test Title</title>\n<subtitle>Test Subtitle</subtitle>\n<bibScope type="accession">2011-166</bibScope><bibScope type="unknownScope">abc123</bibScope>\n</bibRef>'
+        bib_ref = self.to_element(xml)
+        self.assertEqual('Test Title, Accession 2011-166, Subtitle Test Subtitle, Unknownscope abc123', BibRefToTextConverter.as_text(bib_ref))
+
+    def test_bib_ref_with_multiple_titles_displays_multiple_titles(self):
+        xml = '<bibRef><title type="main">Test Title 1</title>\n<title type="main">Test Title 2</title>\n<bibScope type="accession">2011-166</bibScope>\n</bibRef>'
+        bib_ref = self.to_element(xml)
+        self.assertEqual('Test Title 1, Test Title 2, Accession 2011-166', BibRefToTextConverter.as_text(bib_ref))
+
+    def test_bib_ref_with_multiple_bibscopes_displays_multiple_entries(self):
+        xml = '<bibRef><title type="main">Test Title</title>\n<bibScope type="accession">2011-166</bibScope><bibScope type="accession">ABC-123</bibScope></bibRef>'
+        bib_ref = self.to_element(xml)
+        self.assertEqual('Test Title, Accession 2011-166, Accession ABC-123', BibRefToTextConverter.as_text(bib_ref))
 
 if __name__ == '__main__':
     unittest.main()
